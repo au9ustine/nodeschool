6fb270aae743c81c1cce53fcb1b212eaec125edf
au9ustine <duke.augustine@gmail.com>
2018-02-25 12:50:57 +0800

    core :: stream-adventure :: html-stream :: completed

---
+++ core-stream-adventure/10-html-stream.js
@@ -0,0 +1,10 @@
+const trumpet = require('trumpet');
+const through = require('through2');
+let tr = trumpet();
+let loud = tr.select('.loud').createStream();
+loud.pipe(through(function (chunk, _, next) {
+    this.push(chunk.toString().toUpperCase());
+    next();
+})).pipe(loud);
+
+process.stdin.pipe(tr).pipe(process.stdout);
