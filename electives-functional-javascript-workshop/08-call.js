fae8c7232c27488138d0fd9ae7a053ee9c382819
au9ustine <duke.augustine@gmail.com>
2018-05-05 10:16:38 +0800

    electives :: functional javascript :: 08 Call :: completed

---
+++ electives-functional-javascript-workshop/08-call.js
@@ -0,0 +1,7 @@
+function duckCount() {
+    return Array.prototype.slice.call(arguments).filter((x) =>
+        Object.prototype.hasOwnProperty.call(x, 'quack')
+    ).length;
+}
+
+module.exports = duckCount;
