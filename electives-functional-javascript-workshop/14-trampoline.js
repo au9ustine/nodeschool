29c1d394e3d8e25bc0febd466fef67ca556aeda3
au9ustine <duke.augustine@gmail.com>
2018-05-08 23:40:32 +0800

    electives :: functional javascript :: 14 Trampoline :: completed

---
+++ electives-functional-javascript-workshop/14-trampoline.js
@@ -0,0 +1,21 @@
+// Quoted from https://github.com/timoxley/functional-javascript-workshop/blob/master/exercises/trampoline/solution/solution.js
+
+function repeat(operation, num) {
+    return function () {
+        if (num <= 0) return;
+        operation();
+        return repeat(operation, --num);
+    };
+}
+
+function trampoline(fn) {
+    while (fn && typeof fn === 'function') {
+        fn = fn();
+    }
+}
+
+module.exports = function (operation, num) {
+    trampoline(function () {
+        return repeat(operation, num);
+    });
+};
