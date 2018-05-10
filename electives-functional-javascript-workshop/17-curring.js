522b74b79a437333c3ab1fa3ef83ca9facd13049
au9ustine <duke.augustine@gmail.com>
2018-05-10 22:40:37 +0800

    electives :: functional javascript :: 17 Currying :: completed

---
+++ electives-functional-javascript-workshop/17-curring.js
@@ -0,0 +1,10 @@
+// Quoted from https://github.com/timoxley/functional-javascript-workshop/blob/master/exercises/currying/solution/solution.js
+
+function curryN(fn, n) {
+    n = n || fn.length;
+    return function curriedN(arg) {
+        if (n <= 1) return fn(arg);
+        return curryN(fn.bind(this, arg), n - 1);
+    };
+}
+module.exports = curryN;
